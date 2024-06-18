56387696f492953dd2cbbb232d213a29c001eafb
Srinivasan, Balajee Devesha <sbdevesha1998@gmail.com>
2024-06-18 03:05:42 -0400

    Updated the the admin side, all working, now adding the user side

---
+++ User/app.py
@@ -0,0 +1,59 @@
+import boto3  # type: ignore
+import streamlit as st  # type: ignore
+import os 
+import uuid
+
+## S3_client
+BUCKET_NAME = os.getenv("BUCKET_NAME")
+AWS_REGION = os.getenv("AWS_REGION")
+
+S3_client = boto3.client("s3",region_name= AWS_REGION)
+
+## Bedrock
+from langchain_community.embeddings import BedrockEmbeddings # type: ignore
+
+## Text Splitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter# type: ignore
+ 
+
+ ## PDF loader 
+from langchain_community.document_loaders import PyPDFLoader# type: ignore
+
+## Unique ID generation 
+def get_unique_id():
+    return str(uuid.uuid4())
+
+## splitting the pages and texts 
+def split_text(pages, chunk_size, chunk_overlap):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    docs = splitter.split_documents(pages)
+    return docs
+
+##import FAISS
+from langchain_community.vectorstores import FAISS # type: ignore
+bedrock_client = boto3.client(service_name="bedrock-runtime",region_name= AWS_REGION )
+bedrock_embedding  = BedrockEmbeddings(model_id = "amazon.titan-embed-text-v1", client = bedrock_client)
+
+
+
+## Creating the vector store
+def create_vector_store(request_id, documents):
+    vector_store_faiss = FAISS.from_documents( documents, bedrock_embedding)
+    FileName= f"{request_id}.bin"
+    FolderPath = "/tmp/"
+    vector_store_faiss.save_local(index_name= FileName,folder_path= FolderPath, )
+
+    ## Upload the file to S3
+    S3_client.upload_file(FolderPath +'/' + FileName + '.faiss', Bucket=  BUCKET_NAME, Key = 'my_faiss.faiss')
+    S3_client.upload_file(FolderPath +'/' + FileName + '.pkl', Bucket=  BUCKET_NAME, Key = 'my_faiss.pkl')
+
+    return True
+
+
+def main():
+    st.header("This is the Client side for pdf chat demo using Bedrock Titan embeddings")
+    
+
+
+if __name__ == "__main__":
+    main()
